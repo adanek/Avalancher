71ef14249aa9887a4f9d1be6fff110e297a3fafb
adanek <andreas.danek-bulius@student.uibk.ac.at>
2016-01-28 01:23:40 +0100

    Client Riskreport

---
+++ public/scripts/components/hero-form.component.ts
@@ -0,0 +1,20 @@
+import {Input, Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+import {Hero} from '../models/hero';
+
+@Component({
+    selector: 'hero-form',
+    inputs: ['[(hero)]'],
+    templateUrl: 'views/hero-form.component.html',
+    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
+})
+export class HeroFormComponent {
+    @Input('hero') hero:Hero;
+    powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+    submitted = false;
+
+    onSubmit() {
+        this.submitted = true;
+    }
+
+    get diagnostic(){return JSON.stringify(this.hero);}
+}
---
+++ public/scripts/models/ski-area.ts
@@ -0,0 +1,8 @@
+export class SkiArea{
+    public name: string;
+    public city: string;
+    public avalancherisk: string;
+    public snowheight: number;
+    public openLiftCount: number;
+    public liftCount: number;
+}
